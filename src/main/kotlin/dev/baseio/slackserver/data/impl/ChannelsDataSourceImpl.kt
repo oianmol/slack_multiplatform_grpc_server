5faac4f76c588a1a64b728cf3d10ef505bb30d81
anmol verma <anmol.verma4@gmail.com>
2022-10-15 15:42:42 +0530

    remove: unused imports

--- src/main/kotlin/dev/baseio/slackserver/data/impl/ChannelsDataSourceImpl.kt
+++ src/main/kotlin/dev/baseio/slackserver/data/impl/ChannelsDataSourceImpl.kt
@@ -1,29 +1,18 @@
 package dev.baseio.slackserver.data.impl
 
-import com.mongodb.client.MongoCursor
-import com.mongodb.client.MongoDatabase
-import com.mongodb.client.model.Aggregates
 import com.mongodb.client.model.Filters
-import com.mongodb.client.model.changestream.ChangeStreamDocument
 import com.mongodb.client.model.changestream.OperationType
 import dev.baseio.slackserver.data.ChannelsDataSource
 import dev.baseio.slackserver.data.SkChannel
 import io.grpc.Status
 import io.grpc.StatusException
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import org.bson.Document
 import org.bson.conversions.Bson
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.eq
-import org.litote.kmongo.getCollection
 import org.litote.kmongo.match
-import org.litote.kmongo.or
-import java.util.Collections.singletonList
-import java.util.stream.Stream
-
 
 class ChannelsDataSourceImpl(private val slackCloneDB: CoroutineDatabase) : ChannelsDataSource {
   override fun getChannelChangeStream(workspaceId: String): Flow<Pair<SkChannel?, SkChannel?>> {
