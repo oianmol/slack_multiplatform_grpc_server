5fb748f4d7e7750ef1405d0e8a7d7c171483f619
anmol verma <anmol.verma4@gmail.com>
2022-12-04 10:26:51 +0530

    add: SlackServer object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,7 @@ import dev.baseio.slackserver.data.models.SkMessage
 import dev.baseio.slackserver.dataSourcesModule
 import dev.baseio.slackserver.services.*
 import dev.baseio.slackserver.services.interceptors.AuthInterceptor
+import io.grpc.Server
 import io.grpc.ServerBuilder
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.koin.core.context.startKoin
@@ -15,71 +16,70 @@ import org.koin.core.qualifier.named
 import org.koin.java.KoinJavaComponent.getKoin
 import java.security.Security
 
+object SlackServer {
 
-const val TLS_CERT_PATH_OPTION = "tls/tls.crt"
-const val TLS_PRIVATE_KEY_PATH_OPTION = "tls/tls.key"
-
-fun main() {
+  init {
     Security.addProvider(
-        BouncyCastleProvider()
+      BouncyCastleProvider()
     )
     initializeFCM()
 
     initKoin()
+  }
 
-    // The {certificate, private key} pair to use for gRPC TLS.
-    val tlsCertFile = object {}.javaClass.getResourceAsStream(TLS_CERT_PATH_OPTION)
-    val tlsPrivateKeyFile = object {}.javaClass.getResourceAsStream(TLS_PRIVATE_KEY_PATH_OPTION)
-
-    ServerBuilder.forPort(8081)
+  fun start(): Server {
+    return ServerBuilder.forPort(8081)
         //.useTransportSecurity(tlsCertFile, tlsPrivateKeyFile) // TODO enable this once the kmp library supports this.
         .addService(
-            AuthService(
-                pushTokenDataSource = getKoin().get(),
-                authenticationDelegate = getKoin().get()
-            )
+          AuthService(
+            pushTokenDataSource = getKoin().get(),
+            authenticationDelegate = getKoin().get()
+          )
         )
         .addService(QrCodeService(database = getKoin().get(), qrCodeGenerator = getKoin().get()))
         .addService(
-            WorkspaceService(
-                workspaceDataSource = getKoin().get(),
-                authDelegate = getKoin().get()
-            )
+          WorkspaceService(
+            workspaceDataSource = getKoin().get(),
+            authDelegate = getKoin().get()
+          )
         )
         .addService(
-            ChannelService(
-                channelsDataSource = getKoin().get(),
-                channelMemberDataSource = getKoin().get(),
-                usersDataSource = getKoin().get(),
-                channelMemberPNSender = getKoin().get(named(SkChannelMember::class.java.name)),
-                channelPNSender = getKoin().get(named(SkChannel::class.java.name))
-            )
+          ChannelService(
+            channelsDataSource = getKoin().get(),
+            channelMemberDataSource = getKoin().get(),
+            usersDataSource = getKoin().get(),
+            channelMemberPNSender = getKoin().get(named(SkChannelMember::class.java.name)),
+            channelPNSender = getKoin().get(named(SkChannel::class.java.name))
+          )
         )
         .addService(
-            MessagingService(
-                messagesDataSource = getKoin().get(),
-                pushNotificationForMessages = getKoin().get(named(SkMessage::class.java.name))
-            )
+          MessagingService(
+            messagesDataSource = getKoin().get(),
+            pushNotificationForMessages = getKoin().get(named(SkMessage::class.java.name))
+          )
         )
         .addService(UserService(usersDataSource = getKoin().get()))
         .intercept(AuthInterceptor())
         .build()
         .start()
-        .awaitTermination()
+  }
+}
 
-    stopKoin()
+fun main() {
+  SlackServer.start().awaitTermination()
+  stopKoin()
 }
 
 private fun initKoin() {
-    startKoin {
-        modules(dataSourcesModule)
-    }
+  startKoin {
+    modules(dataSourcesModule)
+  }
 }
 
 fun initializeFCM() {
-    val options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.getApplicationDefault())
-        .build()
+  val options = FirebaseOptions.builder()
+    .setCredentials(GoogleCredentials.getApplicationDefault())
+    .build()
 
-    FirebaseApp.initializeApp(options)
+  FirebaseApp.initializeApp(options)
 }
